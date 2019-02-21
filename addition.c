d2a0e10682e432ffa163b7ea6c25d7346127eb4c
yamunamohan <36476374+yamunamohan@users.noreply.github.com>
2019-02-21 12:16:31 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,8 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-int sum,a=10,b=20;
-sum=a+b;
-printf("%d",sum);
-return 0;
+     unsigned int swapped,x = 0x76543210;
+  char *c = (char*) &x;
+
+  printf ("*c is: 0x%x\n", *c);
+  if (*c == 0x10)
+  {
+    printf ("Underlying architecture is little endian. \n");
+  }
+  else
+  {
+     printf ("Underlying architecture is big endian. \n");
+  }
+
+  swapped=((x>>24)&0xff)|((x>>8)&0xff00)|((x<<8)&0xff0000)|((x<<24)&0xff000000);
+
+    printf ("swapped: 0x%x\n", swapped);
+     printf("muxmaga");
+  return 0;
 }
